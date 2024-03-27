43bb45b287a701bffd6bdce7950843a40696d455
Abutalib (Barish) Namazov <abutalib@mit.edu>
2024-03-27 15:41:17 -0400

    Unnecessary export

--- composables/useFetchy.ts
+++ composables/useFetchy.ts
@@ -20,7 +20,7 @@ interface UseFetchyOptions<
   suppress?: boolean;
 }
 
-export type PickFrom<T, K extends Array<string>> = T extends Array<any>
+type PickFrom<T, K extends Array<string>> = T extends Array<any>
   ? T
   : T extends Record<string, any>
   ? keyof T extends K[number]
@@ -30,7 +30,7 @@ export type PickFrom<T, K extends Array<string>> = T extends Array<any>
     : Pick<T, K[number]>
   : T;
 
-export type KeysOf<T> = Array<
+type KeysOf<T> = Array<
   T extends T ? (keyof T extends string ? keyof T : never) : never
 >;
 
